140cf501f82ec6d4f6b336d2ad1d273198fddb5d
Raga-M <ragavendran200@gmail.com>
2022-07-29 15:52:49 +0530

    package importing

---
+++ BankBal.java
@@ -0,0 +1,11 @@
+package sbi.bank;
+import indian.bank.Bank;
+public class BankBal{
+ public static void main(String[] args)
+{
+Bank b=new Bank();   
+b.withdraw(100);
+b.balance(1000,100);
+       
+    }
+}
